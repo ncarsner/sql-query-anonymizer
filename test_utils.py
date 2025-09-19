a5a941338fade9e2f1f4dbc4c807d04faed2866f
Nicholas Carsner <nicholascarsner@gmail.com>
2025-09-19 08:36:18 -0500

    Updated regex args

--- test_utils.py
+++ test_utils.py
@@ -2,10 +2,11 @@ from utils import preprocess_text
 import pytest
 
 @pytest.mark.parametrize("input_text, expected_output", [
-    ("  Hello   World!  ", "hello world!"),
-    ("This   is a   Test.", "this IS a test."),
+    ("  Hello   World!  ", "hello world !"),
+    ("This   is a   Test.", "this IS a test ."),
     ("  MULTIPLE    SPACES   ", "multiple spaces"),
     ("NoExtraSpaces", "noextraspaces"),
+    ("SELECT p.column from table p where p.id = 10", "SELECT p . column FROM table p WHERE p . id = 10"),
     # (" select id  from  table where column  = ' value ';", "SELECT id FROM table WHERE column =' value ' ;"),
 ])
 def test_preprocess_text(input_text, expected_output):
