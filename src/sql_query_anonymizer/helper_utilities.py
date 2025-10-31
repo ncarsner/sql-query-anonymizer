847aae0f60c725b9c56ed230fee288e07599e941
Nicholas Carsner <nicholascarsner@gmail.com>
2025-10-31 14:03:28 -0500

    Split with callable instead of line break char

--- src/sql_query_anonymizer/helper_utilities.py
+++ src/sql_query_anonymizer/helper_utilities.py
@@ -3,7 +3,7 @@ def read_sql_file(filepath: str) -> str:
         sql_statement = f.read()
 
     # Ignore lines that start with '--' (SQL comments)
-    lines = sql_statement.split("\n")
+    lines = sql_statement.splitlines()
     filtered_lines = []
     for line in lines:
         if line.strip().startswith("--"):
